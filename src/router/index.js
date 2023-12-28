efc11bdf80ffc6774ec83a12e1dd41011423f3c5
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2023-12-28 18:16:03 +0700

    path admin

--- src/router/index.js
+++ src/router/index.js
@@ -40,17 +40,23 @@ const router = createRouter({
       component: () => import('../views/ThanksView.vue')
     },
     {
-      path: '/admin/kitchen',
-      name: 'kitchen',
-      component: () => import('../views/admin/Kitchen.vue')
+      path: '/admin',
+      name: 'admin',
+      children: [
+        {
+          path: 'kitchen',
+          name: 'kitchen',
+          component: () => import('../views/admin/Kitchen.vue')
+        },
+        {
+          path: 'orders',
+          name: 'orders',
+          component: () => import('../views/admin/Orders.vue')
+        }
+      ]
     },
-    {
-      path: '/admin/orders',
-      name: 'orders',
-      component: () => import('../views/admin/Orders.vue')
-    }
-    
   ]
+
 })
 
 export default router
